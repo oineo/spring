59f8c301b81281c50190a115b0bcdb12dbefb52f
oineo <oineo.h@gmail.com>
2017-02-21 17:53:57 +0900

    initial the project

---
+++ src/main/java/ini/spring/Application.java
@@ -0,0 +1,12 @@
+package ini.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
